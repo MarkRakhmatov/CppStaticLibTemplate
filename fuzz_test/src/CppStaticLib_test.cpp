152e7319d2fd3a9daa912c1508a4cb5d1e90b838
Mark <mark.rakhmatov@gmail.com>
2025-11-29 23:33:41 +0200

    add compilation flags and add code to init and run fuzztests

--- fuzz_test/src/CppStaticLib_test.cpp
+++ fuzz_test/src/CppStaticLib_test.cpp
@@ -1,5 +1,8 @@
 #include <boost/ut.hpp>
 #include "fuzztest/fuzztest.h"
+#include "fuzztest/init_fuzztest.h"
+#include "absl/debugging/failure_signal_handler.h"
+#include "absl/debugging/symbolize.h"
 
 #include <CppStaticLib/CppStaticLib.hpp>
 #include <iostream>
@@ -16,9 +19,30 @@ void factorialAlwaysGreaterThan0(int input) {
 
 FUZZ_TEST(TestSuite, factorialAlwaysGreaterThan0);
 
-int main() {
+
+void initFuzztest(int argc, char** argv) {
+  absl::InitializeSymbolizer(argv[0]);
+  absl::FailureSignalHandlerOptions options;
+  options.call_previous_handler = true;
+  absl::InstallFailureSignalHandler(options);
+  // We call fuzztest::ParseAbslFlags rather than absl::ParseCommandLine
+  // since the latter would complain about any unknown flags that need
+  // to be passed to legacy fuzzing engines (e.g. libfuzzer).
+  fuzztest::ParseAbslFlags(argc, argv);
+  fuzztest::InitFuzzTest(&argc, &argv);
+}
+
+void runFuzztests() {
+  for (const auto& name: fuzztest::ListRegisteredTests()) {
+    fuzztest::RunSpecifiedFuzzTest(name, {});
+  }
+}
+
+int main(int argc, char** argv) {
   try {
+    initFuzztest(argc, argv);
     factorialAlwaysGreaterThan0(5);
+    runFuzztests();
   } catch (const std::exception& ex) {
     std::cout << "exception: "<< ex.what();
     return EXIT_FAILURE;
