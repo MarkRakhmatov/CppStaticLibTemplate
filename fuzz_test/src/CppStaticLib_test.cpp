5ac2b2c57f733261ab06de09feb491bace765ccd
Mark <mark.rakhmatov@gmail.com>
2025-11-29 21:36:40 +0200

    add fuzz test

--- fuzz_test/src/CppStaticLib_test.cpp
+++ fuzz_test/src/CppStaticLib_test.cpp
@@ -1,35 +1,24 @@
 #include <boost/ut.hpp>
+#include "fuzztest/fuzztest.h"
 
 #include <CppStaticLib/CppStaticLib.hpp>
-#include <string>
 #include <iostream>
 #include <cstdlib>
 #include <exception>
 
-void tests() {
+void factorialAlwaysGreaterThan0(int input) {
   using namespace boost::ut;
 
-  "get string"_test = [] {
-    expect(!csl::getString().empty());
-    expect(csl::getString() == std::string("cpp static lib example"));
-  };
-
-  "factorial"_test = [] {
-    expect(csl::factorial(0) == 1);
-    expect(csl::factorial(1) == 1);
-    expect(csl::factorial(2) == 2);
-  };
-
-  "factorial_constexpr"_test = [] {
-    static_assert(csl::factorialConstexpr(0) == 1);
-    static_assert(csl::factorialConstexpr(1) == 1);
-    static_assert(csl::factorialConstexpr(2) == 2);
+  "factorial"_test = [input] {
+    expect(csl::factorial(input) > 0);
   };
 }
 
+FUZZ_TEST(TestSuite, factorialAlwaysGreaterThan0);
+
 int main() {
   try {
-    tests();
+    factorialAlwaysGreaterThan0(5);
   } catch (const std::exception& ex) {
     std::cout << "exception: "<< ex.what();
     return EXIT_FAILURE;
@@ -38,4 +27,6 @@ int main() {
     std::cout <<"unknown exception";
     return EXIT_FAILURE;
   }
+
+  return EXIT_SUCCESS;
 }
