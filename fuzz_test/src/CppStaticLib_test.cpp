6185878c75074a292ad0424d0e1ecadec873128c
Mark <mark.rakhmatov@gmail.com>
2025-12-01 13:49:18 +0200

    use gtest for fuzz tests

--- fuzz_test/src/CppStaticLib_test.cpp
+++ fuzz_test/src/CppStaticLib_test.cpp
@@ -1,66 +1,20 @@
-#include <boost/ut.hpp>
 #include "fuzztest/fuzztest.h"
-#include "fuzztest/init_fuzztest.h"
+#include "gtest/gtest.h"
 #include "absl/debugging/failure_signal_handler.h"
 #include "absl/debugging/symbolize.h"
 
 #include <CppStaticLib/CppStaticLib.hpp>
-#include <iostream>
 #include <cstdlib>
-#include <exception>
-#include <filesystem>
 
-using namespace boost::ut;
-
-void unitTests() {
-  "ut_factorial_3"_test = [] {
-    expect(csl::factorial(3) == 6_i);
-  };
+void factorialAlwaysGreaterThan0OrInvalid(int i) {
+  auto res = csl::factorial(i);
+  EXPECT_TRUE(res > 0 || res == -1 );
 }
 
-void factorialAlwaysGreaterThan0OrInvalid(int) {
+TEST(FactorialFuzzTestSuite, factorial2) {
+  EXPECT_EQ(csl::factorial(2), 2);
 }
 
 FUZZ_TEST(FactorialFuzzTestSuite, factorialAlwaysGreaterThan0OrInvalid);
 
 
-void initFuzztest(int argc, char** argv, std::string_view binary_id) {
-  absl::InitializeSymbolizer(argv[0]);
-  absl::FailureSignalHandlerOptions options;
-  options.call_previous_handler = true;
-  absl::InstallFailureSignalHandler(options);
-  // We call fuzztest::ParseAbslFlags rather than absl::ParseCommandLine
-  // since the latter would complain about any unknown flags that need
-  // to be passed to legacy fuzzing engines (e.g. libfuzzer).
-  fuzztest::ParseAbslFlags(argc, argv);
-  fuzztest::InitFuzzTest(&argc, &argv, binary_id);
-}
-
-void runFuzztests(std::string_view binary_id) {
-  for (const auto& name: fuzztest::ListRegisteredTests()) {
-    fuzztest::RunSpecifiedFuzzTest(name, binary_id);
-  }
-}
-
-int main(int argc, char** argv) {
-  try {
-    const char* exePath = argv[0];
-    std::filesystem::path fullPath{exePath};
-    auto binary_id = fullPath.filename().string();
-    boost::ut::detail::cfg::parse_arg_with_fallback(argc, const_cast<const char**>(argv));
-    unitTests();
-
-    initFuzztest(argc, argv, binary_id);
-    runFuzztests(binary_id);
-  } catch (const std::exception& ex) {
-    std::cout << "exception: "<< ex.what();
-    return EXIT_FAILURE;
-  }
-  catch (...) {
-    std::cout <<"unknown exception";
-    return EXIT_FAILURE;
-  }
-
-  std::cout <<"Success";
-  return EXIT_SUCCESS;
-}
