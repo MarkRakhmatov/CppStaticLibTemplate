f5559f29172fb1631486ba4fedc1bde8bae947ea
Mark <mark.rakhmatov@gmail.com>
2025-12-01 20:53:47 +0200

    fix crash and add one more test

--- fuzz_test/src/CppStaticLib_test.cpp
+++ fuzz_test/src/CppStaticLib_test.cpp
@@ -6,15 +6,21 @@
 #include <CppStaticLib/CppStaticLib.hpp>
 #include <cstdlib>
 
+TEST(FactorialFuzzTestSuite, factorial2) {
+  EXPECT_EQ(csl::factorial(2), 2);
+}
+
 void factorialAlwaysGreaterThan0OrInvalid(int i) {
   auto res = csl::factorial(i);
   EXPECT_TRUE(res > 0 || res == -1 );
 }
 
-TEST(FactorialFuzzTestSuite, factorial2) {
-  EXPECT_EQ(csl::factorial(2), 2);
-}
-
 FUZZ_TEST(FactorialFuzzTestSuite, factorialAlwaysGreaterThan0OrInvalid);
 
 
+void factorialWithNegativeInput(int i) {
+  auto res = csl::factorial(-i);
+  EXPECT_TRUE(res > 0 || res == -1 );
+}
+
+FUZZ_TEST(FactorialFuzzTestSuite, factorialWithNegativeInput);
